7b59618b4bd4fecd3b60ed6e801ed6fbdb92083b
Simon Münker <56649035+smnmnkr@users.noreply.github.com>
2021-12-23 10:52:24 +0100

    added additional preprocessing steps

--- lib/preprocessor.py
+++ lib/preprocessor.py
@@ -14,7 +14,17 @@ class Preprocessor:
         """
         Init with custom pipeline or default all preprocessing steps.
 
-        :param list pipeline: ['lowercase', 'hyperlinks', 'mentions', 'hashtags', 'emojis']
+        :param list pipeline: [
+            'lowercase',
+            'hyperlinks',
+            'mentions',
+            'hashtags',
+            'retweet',
+            'repetitions',
+            'emojis',
+            'smileys',
+            'spaces'
+        ]
         """
 
         if pipeline is None:
@@ -56,7 +66,11 @@ class Preprocessor:
             'hyperlinks': self.hyperlinks,
             'mentions': self.mentions,
             'hashtags': self.hashtags,
+            'retweet': self.retweet,
             'emojis': self.emojis,
+            'smileys': self.smileys,
+            'repetitions': self.repetitions,
+            'spaces': self.spaces,
         }
 
     #  -------- lowercase -----------
@@ -76,43 +90,79 @@ class Preprocessor:
     @staticmethod
     def hyperlinks(text: str) -> str:
         """
-        Removes hyperlinks
+        Removes hyperlinks, replaces with token url
 
         :param text: str
         :return: text: str
         """
-        return re.sub(r'\S*https?:\S*', ' ', text)
+        return re.sub(r'\S*https?:\S*', ' url ', text)
 
     #  -------- mentions -----------
     #
     @staticmethod
     def mentions(text: str) -> str:
         """
-        Removes mentions
+        Removes mentions, replaces with token mention
 
         :param text: str
         :return: text: str
         """
-        return re.sub(r'@\w*', ' ', text)
+        return re.sub(r'@\w*', ' mention ', text)
 
     #  -------- hashtags -----------
     #
     @staticmethod
     def hashtags(text: str) -> str:
         """
-        Removes hashtags
+        Removes hashtags, replaces with hashtag content
 
         :param text: str
         :return: text: str
         """
-        return re.sub(r'#\w*', "", text)
+        return re.sub(r'#(\S+)', r' \1 ', text)
+
+    #  -------- retweet -----------
+    #
+    @staticmethod
+    def retweet(text: str) -> str:
+        """
+        Removes retweets
+
+        :param text: str
+        :return: text: str
+        """
+        return re.sub(r'\brt\b', ' ', text)
+
+    #  -------- repetitions -----------
+    #
+    @staticmethod
+    def repetitions(text: str) -> str:
+        """
+        Convert more than 2 letter repetitions to 2 letter
+
+        :param text: str
+        :return: text: str
+        """
+        return re.sub(r'(.)\1+', r'\1\1', text)
+
+    #  -------- space -----------
+    #
+    @staticmethod
+    def spaces(text: str) -> str:
+        """
+        Normalize spaces
+
+        :param text: str
+        :return: text: str
+        """
+        return re.sub(r'\s+', ' ', text)
 
     #  -------- emojis -----------
     #
     @staticmethod
     def emojis(text: str) -> str:
         """
-        Removes emojis
+        Removes emojis, replaces with token emoji
         src: https://stackoverflow.com/questions/33404752/removing-emojis-from-a-string-in-python
 
         :param text: str
@@ -125,4 +175,35 @@ class Preprocessor:
                              u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
                              "]+", flags=re.UNICODE)
 
-        return pattern.sub(r'<emoji>', text)
+        return pattern.sub(r' emoji ', text)
+
+    #  -------- smileys -----------
+    #
+    @staticmethod
+    def smileys(text: str) -> str:
+        """
+        Removes smileys, replace with token smiley
+        src: https://github.com/abdulfatir/twitter-sentiment-analysis/blob/master/code/preprocess.py
+
+        :rtype: object
+        """
+
+        # Smile -- :), : ), :-), (:, ( :, (-:, :')
+        text = re.sub(r'(:\s?\)|:-\)|\(\s?:|\(-:|:\'\))', ' smiley ', text)
+
+        # Laugh -- :D, : D, :-D, xD, x-D, XD, X-D
+        text = re.sub(r'(:\s?D|:-D|x-?D|X-?D)', ' smiley ', text)
+
+        # Love -- <3, :*
+        text = re.sub(r'(<3|:\*)', ' smiley ', text)
+
+        # Wink -- ;-), ;), ;-D, ;D, (;,  (-;
+        text = re.sub(r'(;-?\)|;-?D|\(-?;)', ' smiley ', text)
+
+        # Sad -- :-(, : (, :(, ):, )-:
+        text = re.sub(r'(:\s?\(|:-\(|\)\s?:|\)-:)', ' smiley ', text)
+
+        # Cry -- :,(, :'(, :"(
+        text = re.sub(r'(:,\(|:\'\(|:"\()', ' smiley ', text)
+
+        return text
