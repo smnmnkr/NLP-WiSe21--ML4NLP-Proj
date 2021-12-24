2e98179c398cc2fe5cce10fdd750060074f6f5d5
Simon Münker <56649035+smnmnkr@users.noreply.github.com>
2021-12-24 09:29:38 +0100

    added evaluation tasks

---
+++ lib/tasks/evaluate.py
@@ -0,0 +1,16 @@
+import torch.nn as nn
+
+from lib.data import batch_loader
+
+
+#
+#
+#  -------- evaluate -----------
+#
+def evaluate(model: nn.Module, data_set) -> None:
+    print("\n[--- EVALUATION ---]")
+
+    test_loader = batch_loader(data_set)
+
+    model.evaluate(test_loader)
+    model.metric.show()
