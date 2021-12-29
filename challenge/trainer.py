46c1e23668e4bf01e22d7ed30083ffcb27d91a26
Simon Münker <56649035+smnmnkr@users.noreply.github.com>
2021-12-29 15:51:07 +0100

    fixed evaluation bug, switched to correct method

--- challenge/trainer.py
+++ challenge/trainer.py
@@ -112,7 +112,7 @@ class Trainer:
                 eval_loss: float = 0.0
                 eval_f1: float = 0.0
                 for idx, batch in self._load_iterator(self.data["eval"], epoch=epoch, desc="Eval"):
-                    eval_f1, eval_loss = self._train(batch, idx, eval_f1, eval_loss)
+                    eval_f1, eval_loss = self._eval(batch, idx, eval_f1, eval_loss)
 
                 # --- ---------------------------------
                 # --- update state
